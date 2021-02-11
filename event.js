07db4a4264bfdb4bfdc6025613a55c9046c360bf
David <David0nike@gmail.com>
2021-02-10 20:47:59 -0400

    Cambios

---
+++ event.js
@@ -0,0 +1,6 @@
+"use strict"
+
+function n() {
+    alert("Estamos en mantenimiento mi pana :v!!");
+
+}
--- parchis.js
+++ parchis.js
@@ -1,8 +1,9 @@
 var dado1 = 0;
 var dado2 = 0;
+var turno;
 
 //Jugador A
-var turno = "A";
+
 
 
 
@@ -11,25 +12,86 @@ var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
     41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-    51, 52, 53, 54, 55, 56, 57, 58, 59, 60
+    51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+
 ];
 
 
 
 
 function CambiarTurno() {
-    if (turno === "A")
+
+    if (turno === "A") {
         turno = "B";
-    else
+    } else {
         turno = "A";
+    }
+    return turno;
 }
+
+function MoverFicha(ficha, posicion) {
+
+    var PuedeMover = false;
+
+    if (turno === "A") {
+        console.log(ficha.value);
+        var ficha1 = document.getElementById(ficha);
+        let espacio = document.getElementById(posicion);
+        espacio.appendChild(ficha1);
+        console.log(espacio.childNodes);
+
+    } else {
+        console.log(espacio.childNodes);
+
+
+    }
+
+
+}
+
+
 
 
 
 function TirarDados() {
     dado1 = Math.round(Math.random() * 5 + 1);
     dado2 = Math.round(Math.random() * 5 + 1);
-    document.getElementById("Dado1").innerHTML = dado1;
-    document.getElementById("Dado2").innerHTML = dado2;
-    CambiarTurno();
+    let diceOne = "Images/" + dado1 + ".jpg";
+    document.querySelector('.img1').setAttribute('src', diceOne)
+    let diceTwo = "Images/" + dado2 + ".jpg";
+    document.querySelector('.img2').setAttribute('src', diceTwo)
+    let turnoSiguiente = CambiarTurno();
+    console.log(dado1 + dado2);
+
+    if (turno === "A") {
+        if (dado1 + dado2 === 5 || dado1 === 5 || dado2 === 5) {
+            MoverFicha("FICHAa1", dado1 + dado2);
+
+
+        } else if (dado1 == 5 && dado2 == 5) {
+            MoverFicha("FICHAa1", dado1 + dado2);
+            MoverFicha("FICHAa2", dado1 + dado2);
+
+
+        } else if (dado1 == 5) {
+            MoverFicha("FICHAa1", dado1 + dado2);
+
+        } else if (dado2 == 2) {
+
+            MoverFicha("FICHAa1", dado1 + dado2);
+
+        }
+    } else {
+        //if (dado1 === 5 || dado2 === 5) {
+        MoverFicha("FICHAr1", dado1 + dado2);
+
+        //}
+    }
+    console.log(turnoSiguiente);
+
+
+} {
+
+
+
 }
